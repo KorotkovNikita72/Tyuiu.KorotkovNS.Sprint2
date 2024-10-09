c25e0f73dcc34f621e1e780ea9dbbd7da182845a
TRESH 567 <tresh78803@gmail.com>
2024-10-09 16:42:08 +0500

    Task1

--- Tyuiu.KorotkovNS.Sprint2.Task1.V12.Lib/DataService.cs
+++ Tyuiu.KorotkovNS.Sprint2.Task1.V12.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.KorotkovNS.Sprint2.Task1.V12.Lib
 {
-    public class DataService : ISprint2Task1V15
+    public class DataService : ISprint2Task1V12
     {
         public bool[] GetLogicOperations(int a, int b, int c, int d)
         {
